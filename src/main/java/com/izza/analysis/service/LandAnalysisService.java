6f3b46c6c63ab9e3d5e29b482267c9fe200e0e39
nicklee0726 <nicklee0726@gmail.com>
2025-08-08 15:52:32 +0900

    chore: 패키지 추가

--- src/main/java/com/izza/analysis/service/LandAnalysisService.java
+++ src/main/java/com/izza/analysis/service/LandAnalysisService.java
@@ -5,6 +5,7 @@ import com.izza.search.persistent.LandDao;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 
